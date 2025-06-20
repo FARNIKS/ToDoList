d2aabf1492606dec913501cee9327c7bfeaab4da
Miguel Angel Jimenez Fajardo <faniksdo@gmail.com>
2025-06-19 23:29:28 -0500

    Subtasks have been removed and methods have been added to verify the task title and description fields, and task importance has been added.

--- js/script.js
+++ js/script.js
@@ -16,3 +16,38 @@ menu.addEventListener("click", (e) => {
     console.log("links no");
   }
 });
+
+document
+  .getElementById("task-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault(); // Evita recargar la página
+
+    // Capturamos cada valor en su respectiva variable
+    const titulo = document.getElementById("title-todo").value;
+    const descripcion = document.getElementById("description-todo").value;
+    const fechaLimite = document.getElementById("fecha").value;
+    const importancia = document.getElementById("importance").value;
+    const dificultad = document.getElementById("difficulty").value;
+    let errors = "";
+
+    if (titulo === "") {
+      errors += "Debes ingresar in titulo\n";
+    }
+
+    if (descripcion.length > 100) {
+      errors += "La descripcion es mayor a 100 caracteres\n";
+    }
+
+    if (errors !== "") {
+      alert(errors);
+      return;
+    }
+
+    // Aquí ya tienes todo validado
+    console.log(errors);
+    console.log("Título:", titulo);
+    console.log("Descripción:", descripcion);
+    console.log("Fecha límite:", fechaFormateada);
+    console.log("Importancia:", importancia);
+    console.log("Dificultad:", dificultad);
+  });
